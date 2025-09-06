768afaa3be48f274076590761ae9c9ad743708b9
Ebenezer Attipoe <kwameebenezer114@gmail.com>
2025-09-06 08:31:33 +0000

    Initial commit for duplicated project

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer style={{backgroundColor: "white",}}>
+  <p style={{textAlign: "center", font: "18px", color: "#02011dff"}}>© 2023 City Lovers</p>
+</footer>
+  )
+}
+ 
+export default Footer;
---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,8 @@
+ function MainContent(){
+ return (
+  <main style={{textAlign: "center"}}>
+     <p style={{textAlign: "center", font: "18px", color: "white"}}>I love to visit New York, Paris, and Tokyo.</p>
+  </main>
+ )
+ }
+ export default MainContent;
