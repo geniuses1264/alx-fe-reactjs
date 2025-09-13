e8a44b4c4115e34ddac0314e673db11e1ac590e1
Ebenezer Attipoe <kwameebenezer114@gmail.com>
2025-09-13 14:43:26 +0000

    Added a Recipe Sharing Application

---
+++ recipe-sharing-app/src/App.jsx
@@ -0,0 +1,16 @@
+import AddRecipeForm from './components/AddRecipeForm'
+import RecipeList from './components/RecipeList'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+     <AddRecipeForm />  
+      <RecipeList />
+    </>
+  )
+}
+
+export default App
