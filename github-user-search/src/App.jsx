4898886a8100be9e58bae76f9ce4fec707434d08
Ebenezer Attipoe <kwameebenezer114@gmail.com>
2025-09-21 19:46:57 +0000

    Update Search component and Tailwind config

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -7,7 +7,9 @@ function App() {
   return (
     
       <div className="min-h-screen bg-gray-100">
-       
+        <h1 className="text-3xl font-bold text-center p-6  gap-10 underline decoration-indigo-600">
+          GitHub User Search
+        </h1>
      <Search />
     </div>
   )
