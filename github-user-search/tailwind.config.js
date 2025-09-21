ad4efaa03fdfb07289a4417b9cd26f69b1089d70
Ebenezer Attipoe <kwameebenezer114@gmail.com>
2025-09-21 18:58:28 +0000

    Update Search component and Tailwind config

--- github-user-search/tailwind.config.js
+++ github-user-search/tailwind.config.js
@@ -1,5 +1,4 @@
-/** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{js,jsx,ts,tsx}"
@@ -9,4 +8,3 @@ export default {
   },
   plugins: [],
 }
-
