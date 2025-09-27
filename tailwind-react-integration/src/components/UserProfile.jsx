2e8e0c4c7a3e97c94f6d4e076aaf54daa1630054
Ebenezer Attipoe <kwameebenezer114@gmail.com>
2025-09-27 00:00:59 +0000

    Created Profile card

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,6 +1,7 @@
 function UserProfile() {
   return (
-    <div className="user-profile bg-gray-100 p-8 max-w-sm max-auto my-20 rounded-lg" >
+    <div className=" bg-gray-100 p-8 max-w-sm max-auto my-20 rounded-lg" >
+
       <img className="reonded-full w-36 h-36 max-auto" src="https://via.placeholder.com/150" alt="User" />
       <h1 className="text-xl text-blue-800 my-4">John Doe</h1>
       <p className="text-gray-600 text-base">Developer at Example Co. Loves to write code and explore new technologies.</p>
