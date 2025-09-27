74fcba64765a8c59d2b6827df69abc8cb9d38c45
Ebenezer Attipoe <kwameebenezer114@gmail.com>
2025-09-27 09:15:37 +0000

    responsive style base on various screen sizes

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,12 +1,12 @@
 export default function UserProfile() {
   return (
-    <div className=" bg-gray-100 p-8 max-w-sm mx-auto my-20 rounded-lg shadow-lg " >
+    <div className=" sm:p-4 bg-gray-100 md:p-8 max-w-sm mx-auto my-20 rounded-lg shadow-lg sm:max-w-xs md:max-w-sm" >
 
-      <img className="rounded-full w-36 h-36 mx-auto" src="https://via.placeholder.com/150" alt="User" />
+      <img className=" sm:w-24 sm:h-24 rounded-full md:w-36 md:h-36 mx-auto" src="./public/UserPic.jpg" alt="User" />
 
-      <h1 className="text-xl text-blue-800 my-4">John Doe</h1>
+      <h1 className="text-xl text-blue-800 my-4 flex justify-center items-center sm:text-lg md:text-xl "><span>John Doe</span></h1>
 
-      <p className="text-gray-600 text-base">Developer at Example Co. Loves to write code and explore new technologies.</p>
+      <p className="text-gray-600 sm:text-sm md:text-base px-3">Developer at Example Co. Loves to write code and explore new technologies.</p>
     </div>
   );
 }
